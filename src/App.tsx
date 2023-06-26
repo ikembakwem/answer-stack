192224f4b1222fef0b036e072da11279eef99ed9
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-06-26 02:33:01 +0100

    Added error page

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import { SearchPage } from "./ui/pages/SearchPage";
 import { AskPage } from "./ui/pages/AskPage";
 import { SignInPage } from "./ui/pages/SignInPage";
 import { Layout } from "./ui/components/Layout";
+import { ErrorPage } from "./ui/pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "",
     Component: Layout,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, Component: HomePage },
       { path: "/search", Component: SearchPage },
---
+++ src/ui/pages/ErrorPage.tsx
@@ -0,0 +1,7 @@
+import { Page } from "./Page";
+
+export const ErrorPage = () => (
+  <Page title="Sorry 😴, the page you requested for does not exist!">
+    {null}
+  </Page>
+);
