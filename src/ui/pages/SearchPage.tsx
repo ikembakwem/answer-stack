24c388fb0dea63a1d136ae216ef02e79f3dc6902
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-06-26 02:43:14 +0100

    Implement search feature on SearchPage component

--- src/ui/pages/SearchPage.tsx
+++ src/ui/pages/SearchPage.tsx
@@ -1,5 +1,42 @@
+import { useSearchParams } from "react-router-dom";
 import { Page } from "./Page";
+import { useEffect, useState } from "react";
+import {
+  QuestionData,
+  searchQuestion,
+} from "../../QuestionsData";
+import { QuestionList } from "../components/QuestionList";
+import styled from "@emotion/styled";
 
-export const SearchPage = () => (
-  <Page title="Search Results">{null}</Page>
-);
+export const SearchPage = () => {
+  const [searchParams] = useSearchParams();
+  const [questions, setQuestions] = useState<
+    QuestionData[]
+  >([]);
+
+  const search = searchParams.get("criteria") || "";
+
+  useEffect(() => {
+    const doSearch = async (criteria: string) => {
+      const searchResults = await searchQuestion(
+        criteria
+      );
+      setQuestions(searchResults);
+    };
+
+    doSearch(search);
+  }, [search]);
+  return (
+    <Page title="Search Results">
+      {search && (
+        <SearchTerm>for "{search}"</SearchTerm>
+      )}
+      <QuestionList data={questions} />
+    </Page>
+  );
+};
+
+const SearchTerm = styled.p`
+  font-size: 16px;
+  font-style: italic;
+`;
