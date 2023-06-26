51194f7d449286ed626747c3dd8de97ce16e2b2a
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-06-26 01:14:34 +0100

    Added routes to pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,34 @@ import styled from "@emotion/styled";
 import { Header } from "./ui/components/Header";
 import { HomePage } from "./ui/pages/HomePage";
 import { fontSize, fontfamily, gray2 } from "./Styles";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { SearchPage } from "./ui/pages/SearchPage";
+import { AskPage } from "./ui/pages/AskPage";
+import { SignInPage } from "./ui/pages/SignInPage";
 
 function App() {
   return (
-    <Wrapper>
-      <Header />
-      <HomePage />
-    </Wrapper>
+    <BrowserRouter>
+      <Wrapper>
+        <Header />
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route
+            path="search"
+            element={<SearchPage />}
+          />
+          <Route path="ask" element={<AskPage />} />
+          <Route
+            path="signin"
+            element={<SignInPage />}
+          />
+        </Routes>
+      </Wrapper>
+    </BrowserRouter>
   );
 }
 
