5409e1cfb85ff210adbd9a8ae287f0f4114ad62e
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-06-27 04:30:10 +0100

    Implemented controlled form

--- src/ui/pages/RegistrationPage.tsx
+++ src/ui/pages/RegistrationPage.tsx
@@ -1,5 +1,104 @@
+import styled from "@emotion/styled";
+import { Button } from "../components/Buttons";
 import { Page } from "./Page";
+import { useState, FormEvent } from "react";
 
-export const RegistrationPage = () => (
-  <Page title="Registration">{null}</Page>
-);
+type Data = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+};
+
+const defaultData: Data = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  username: "",
+};
+
+export const RegistrationPage = () => {
+  const [userData, setUserData] =
+    useState<Data>(defaultData);
+
+  const handleSubmit = (
+    e: FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+    console.log("Submitted data", userData);
+  };
+
+  return (
+    <Page title="Registration">
+      <div>
+        <form onSubmit={handleSubmit}>
+          <FieldStyle>
+            <label htmlFor="firstName">
+              First name
+            </label>
+            <input
+              type="text"
+              id="name"
+              value={userData.firstName}
+              onChange={(e) =>
+                setUserData({
+                  ...userData,
+                  firstName: e.target.value,
+                })
+              }
+            />
+          </FieldStyle>
+          <FieldStyle>
+            <label htmlFor="lastName">Last name</label>
+            <input
+              type="text"
+              id="lastName"
+              value={userData.lastName}
+              onChange={(e) =>
+                setUserData({
+                  ...userData,
+                  lastName: e.target.value,
+                })
+              }
+            />
+          </FieldStyle>
+          <FieldStyle>
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              id="email"
+              value={userData.email}
+              onChange={(e) =>
+                setUserData({
+                  ...userData,
+                  email: e.target.value,
+                })
+              }
+            />
+          </FieldStyle>
+          <FieldStyle>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              value={userData.username}
+              onChange={(e) =>
+                setUserData({
+                  ...userData,
+                  username: e.target.value,
+                })
+              }
+            />
+          </FieldStyle>
+          <Button type="submit">Register</Button>
+        </form>
+      </div>
+    </Page>
+  );
+};
+
+const FieldStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 8px;
+`;
