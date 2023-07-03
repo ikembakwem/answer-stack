d25edc084af19fac3b87a24b38851c1a69a831a8
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-07-03 02:39:39 +0100

    Managed search form using controlled form implementation

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -1,25 +1,39 @@
-/** @JSXImportSource @emotion/react */
 import styled from "@emotion/styled";
 import { UserIcon } from "./Icon";
 import { gray5 } from "../../Styles";
 import { LogoLink, NavLink } from "./Links";
 import { SearchBox } from "./SearchBox";
+import { useSearchParams } from "react-router-dom";
+import { FormEvent, useState } from "react";
 
 export const Header = () => {
+  const [searchParams] = useSearchParams();
+  const criteria = searchParams.get("criteria") || "";
+  const [search, setSearch] = useState(criteria);
+
   const handleSearchInput = (
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
-    console.log(e.currentTarget.value);
+    setSearch(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(search);
   };
   return (
     <Wrapper>
       <LogoLink to="/">Answer Stack</LogoLink>
       <SearchContainer>
-        <SearchBox
-          type="text"
-          placeholder="Search..."
-          onChange={handleSearchInput}
-        />
+        <form onSubmit={handleSubmit}>
+          <SearchBox
+            type="text"
+            placeholder="Search..."
+            name="search"
+            onChange={handleSearchInput}
+            value={search}
+          />
+        </form>
       </SearchContainer>
 
       <NavLink to="/login">
--- src/ui/components/SearchBox.tsx
+++ src/ui/components/SearchBox.tsx
@@ -5,24 +5,25 @@ import {
   gray2,
   gray5,
 } from "../../Styles";
-import { FC, ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
 
-interface InputProps extends ComponentProps<"input"> {}
-
-export const SearchBox: FC<InputProps> = ({
-  ...props
-}) => <InputStyles {...props} />;
+export const SearchBox = forwardRef<
+  HTMLInputElement,
+  ComponentProps<"input">
+>(({ ...props }, ref) => {
+  return <InputStyles ref={ref} {...props} />;
+});
 
 const InputStyles = styled.input`
   font-family: ${fontfamily};
   font-size: ${fontSize};
   padding: 8px 10px;
   border: 1px solid ${gray5};
-  border-radius: 3px;
+  border-radius: 999px;
   color: ${gray2};
   background-color: #fff;
   width: 320px;
-  height: 40px;
+  height: 44px;
   :focus {
     outline-color: ${gray5};
   }
