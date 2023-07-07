5dd6b3ddddb83c5d4d904c8d4bd71e9473418e40
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-07-07 01:09:12 +0100

    Created actions for viewing a question, searching questions and retrieving unanswered questions

--- src/Store.ts
+++ src/Store.ts
@@ -17,3 +17,48 @@ const initialQuestionState: QuestionsState = {
   viewing: null,
   searched: [],
 };
+
+// Actions
+export const GETTINGUNANSWEREDQUESTIONS =
+  "GettingUnansweredQuestions";
+export const gettingUnansweredQuestionsAction = () =>
+  ({
+    type: GETTINGUNANSWEREDQUESTIONS,
+  } as const);
+
+export const GOTUNANSWEREDQUESTIONS =
+  "GotUnansweredQuestions";
+export const gotUnansweredQuestionsAction = (
+  questions: QuestionData[]
+) =>
+  ({
+    type: GOTUNANSWEREDQUESTIONS,
+    questions,
+  } as const);
+
+export const GETTINGQUESTION = "GettingQuestion";
+export const gettingQuestionAction = () => ({
+  type: GETTINGQUESTION,
+});
+
+export const GOTQUESTION = "GotQuestion";
+export const gotQuestionAction = (
+  question: QuestionData | null
+) =>
+  ({
+    type: GOTQUESTION,
+    question,
+  } as const);
+
+export const SEARCHINGQUESTIONS = "SearchingQuestions";
+export const searchingQuestionsAction = () => ({
+  type: SEARCHINGQUESTIONS,
+});
+
+export const SEARCHEDQUESTIONS = "SearchedQuestions";
+export const searchedQuestionsAction = (
+  questions: QuestionData[]
+) => ({
+  type: SEARCHEDQUESTIONS,
+  questions,
+});
